5832bde36163df93b0ceaadfa6dfa94c6cf2c654
Alireza <seyed.alireza.miryazdi@gmail.com>
2022-06-09 02:54:19 +0430

    important notes added

--- config.mk
+++ config.mk
@@ -7,9 +7,12 @@ VERSION = 6.3
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-# 
+# void linux fix
 X11INC = /usr/include/X11
 X11LIB = /usr/lib/X11
+# normally
+# X11INC = /usr/X11R6/include
+# X11LIB = /usr/X11R6/lib
 
 BDINC = /usr/include/fribidi
 
